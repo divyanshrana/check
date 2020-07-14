dc4c7e187a475e098508e901c3ec9dd0be4e69f5
divyanshrana <divyansh.rana12@gmail.com>
2020-07-15 00:17:43 +0530

    Updates

---
+++ precache-manifest.6b82b18211c668427be291865e184b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54edee7692f131359af921b92e5ff490",
+    "url": "/check/index.html"
+  },
+  {
+    "revision": "70a233def21110258c42",
+    "url": "/check/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/check/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/check/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a233def21110258c42",
+    "url": "/check/static/js/main.8caf1b8d.chunk.js"
+  },
+  {
+    "revision": "3ad575ba65fef742a7e8",
+    "url": "/check/static/js/runtime-main.f45e3ce4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/check/static/media/logo.5d5d9eef.svg"
+  }
+]);
